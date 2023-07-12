9a7fc0d806b2a610d8656c429baa946429ceb0d3
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-07-12 15:54:36 +0530

    Include Composite Futures

--- src/main/java/iudx/apd/acl/server/policy/DeletePolicy.java
+++ src/main/java/iudx/apd/acl/server/policy/DeletePolicy.java
@@ -4,6 +4,7 @@ import static iudx.apd.acl.server.apiserver.util.Constants.*;
 import static iudx.apd.acl.server.common.HttpStatusCode.BAD_REQUEST;
 import static iudx.apd.acl.server.policy.utility.Constants.*;
 
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
@@ -32,8 +33,69 @@ public class DeletePolicy {
 
   /* TODO : replace the hardcoded ownerId with the actual ownerId */
 
+  private String getFailureResponse(JsonObject response, String detail) {
+    return response
+        .put(TYPE, BAD_REQUEST.getValue())
+        .put(TITLE, BAD_REQUEST.getUrn())
+        .put(DETAIL, detail)
+        .encode();
+  }
+
+  private Future<Boolean> executeCountQuery(
+      String query, int policyIdSetSize, JsonObject policyJsonObject) {
+    Promise<Boolean> promise = Promise.promise();
+    postgresService.executeQueryWithParams(
+        query,
+        policyJsonObject,
+        handler -> {
+          if (handler.succeeded()) {
+            try {
+              int count =
+                  handler.result().getJsonArray("result").getJsonObject(0).getInteger("count");
+              boolean areTheNumberOfPoliciesEqual = policyIdSetSize == count;
+              if (areTheNumberOfPoliciesEqual) {
+                promise.complete(true);
+              } else {
+                LOG.error("count not equal to list of policies to be deleted");
+                promise.fail(
+                    getFailureResponse(
+                        responseJson,
+                        FAILURE_MESSAGE + ", count not equal to list of policies to be deleted"));
+              }
+            } catch (NullPointerException exception) {
+              LOG.error("Failure : " + handler.cause().getMessage());
+              promise.fail(getFailureResponse(responseJson, FAILURE_MESSAGE));
+            }
+          } else {
+            LOG.error("Query execution failed : " + handler.cause().getMessage());
+            promise.fail(
+                getFailureResponse(responseJson, FAILURE_MESSAGE + ", Query execution failed "));
+          }
+        });
+    return promise.future();
+  }
+
+  private Future<JsonObject> executeUpdateQuery(String query, JsonObject policyJsonObject) {
+    Promise<JsonObject> promise = Promise.promise();
+    postgresService.executeQueryWithParams(
+        query,
+        policyJsonObject,
+        handler -> {
+          if (handler.succeeded()) {
+            LOG.debug("update query succeeded");
+            responseJson = handler.result();
+            responseJson.put(STATUS_CODE, HttpStatusCode.SUCCESS.getValue());
+            promise.complete(responseJson);
+          } else {
+            LOG.debug("update query failed");
+            promise.fail(
+                getFailureResponse(responseJson, FAILURE_MESSAGE + ", update query failed"));
+          }
+        });
+    return promise.future();
+  }
+
   public Future<JsonObject> initiateDeletePolicy(JsonArray policyList, String ownerId) {
-    promise = Promise.promise();
     policyIdSet = new HashSet<>();
     JsonObject policyJsonObject = new JsonObject();
     for (int index = 0; index < policyList.size(); index++) {
@@ -41,7 +103,7 @@ public class DeletePolicy {
       policyJsonObject.put(String.valueOf(index), policy.getString("id"));
       if (policyIdSet.contains(policy.getString("id"))) {
         LOG.error("Duplicate policy Ids");
-        promise.fail(
+        return Future.failedFuture(
             getFailureResponse(responseJson, "Duplicate policy Ids present in the request"));
       }
       policyIdSet.add(policy.getString("id"));
@@ -50,67 +112,25 @@ public class DeletePolicy {
 
     query = COUNT_OF_ACTIVE_POLICIES.replace("$0", POLICY_TABLE_NAME).replace("$2", ownerId);
     finalQuery = DELETE_POLICY_QUERY.replace("$0", POLICY_TABLE_NAME);
-
-    postgresService.executeQueryWithParams(
-        query,
-        policyJsonObject,
-        handler -> {
-          if (handler.succeeded()) {
-            LOG.trace("inside success handler with the result : " + handler.result().encode());
-            if (handler.result() != null
-                && handler.result().getJsonArray("result").getJsonObject(0).getInteger("count")
-                    != null) {
-              int count =
-                  handler.result().getJsonArray("result").getJsonObject(0).getInteger("count");
-              LOG.trace(
-                  "count from the result is : "
-                      + count
-                      + " | policyList size is : "
-                      + policyList.size());
-              if (count == policyList.size()) {
-                postgresService.executeQueryWithParams(
-                    finalQuery,
-                    policyJsonObject,
-                    updateQueryHandler -> {
-                      if (updateQueryHandler.succeeded()) {
-                        LOG.debug("update query succeeded");
-                        responseJson = handler.result();
-                        responseJson.put(STATUS_CODE, HttpStatusCode.SUCCESS.getValue());
-                        promise.complete(responseJson);
-                      } else {
-                        LOG.debug("update query failed");
-                        promise.tryFail(
-                            getFailureResponse(
-                                responseJson, FAILURE_MESSAGE + ", update query failed"));
-                      }
-                    });
-              } else {
-                LOG.error("count not equal to list of policies to be deleted");
-                promise.tryFail(
-                    getFailureResponse(
-                        responseJson,
-                        FAILURE_MESSAGE + ", count not equal to list of policies to be deleted"));
-              }
-            } else {
-              LOG.error("Failure : " + handler.cause().getMessage());
-              promise.tryFail(
-                  getFailureResponse(
-                      responseJson, FAILURE_MESSAGE + ", " + handler.cause().getMessage()));
-            }
-          } else {
-            LOG.error("Query execution failed : " + handler.cause().getMessage());
-            promise.tryFail(
-                getFailureResponse(responseJson, FAILURE_MESSAGE + ", Query execution failed "));
-          }
-        });
-    return promise.future();
-  }
-
-  private String getFailureResponse(JsonObject response, String detail) {
-    return response
-        .put(TYPE, BAD_REQUEST.getValue())
-        .put(TITLE, BAD_REQUEST.getUrn())
-        .put(DETAIL, detail)
-        .encode();
+    var countQueryFuture = executeCountQuery(query, policyIdSet.size(), policyJsonObject);
+    var updateQueryFuture = executeUpdateQuery(finalQuery, policyJsonObject);
+    var resultFuture =
+        CompositeFuture.all(countQueryFuture, updateQueryFuture)
+            .compose(
+                object -> {
+                  if (countQueryFuture.succeeded()) {
+                    if (updateQueryFuture.result() != null
+                        && !updateQueryFuture.result().isEmpty()) {
+                      JsonObject response = updateQueryFuture.result();
+                      response.put(RESULT, "Policy deleted successfully");
+                      return Future.succeededFuture(updateQueryFuture.result());
+                    } else {
+                      return Future.failedFuture(updateQueryFuture.cause().getMessage());
+                    }
+                  } else {
+                    return Future.failedFuture(countQueryFuture.cause().getMessage());
+                  }
+                });
+    return resultFuture;
   }
 }
