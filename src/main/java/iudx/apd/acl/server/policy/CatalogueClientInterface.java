62d4722ea9eeee1db520fc8bcf480b350fd94fb8
Gopal Mahajan <gopalmit2601@gmail.com>
2023-08-08 02:05:07 +0530

    cat-integration

--- src/main/java/iudx/apd/acl/server/policy/CatalogueClientInterface.java
+++ src/main/java/iudx/apd/acl/server/policy/CatalogueClientInterface.java
@@ -1,13 +1,25 @@
 package iudx.apd.acl.server.policy;
 
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-import java.util.HashSet;
+import iudx.apd.acl.server.apiserver.util.ResourceObj;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+/**
+ * Interface for the Catalogue Client, responsible for fetching resource/resource_group from the
+ * Catalogue server and updating the resource_entity table in the database.
+ */
 public interface CatalogueClientInterface {
 
-  Future<Set<UUID>> fetchItem(Set<UUID> id);
-
+  /**
+   * Fetches resource/resource_group from the Catalogue server and updates the resource_entity table in the
+   * database.
+   *
+   * @param ids A set of unique IDs of resource/resource_group to be fetched.
+   * @return A Future containing a list of ResourceObj representing the fetched resources. The
+   *     Future is resolved with the fetched resourceObj list on success, or failed with an error
+   *     message on failure.
+   */
+  Future<List<ResourceObj>> fetchItemDBUpdate(Set<UUID> ids);
 }
