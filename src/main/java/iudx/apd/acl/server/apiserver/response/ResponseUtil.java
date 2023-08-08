e7b6e4d5a1069d7763669c8a1bcb2f0b57c477b9
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-08-08 10:32:31 +0530

    Resolve conflict

--- src/main/java/iudx/apd/acl/server/apiserver/response/ResponseUtil.java
+++ src/main/java/iudx/apd/acl/server/apiserver/response/ResponseUtil.java
@@ -5,19 +5,20 @@ import iudx.apd.acl.server.common.HttpStatusCode;
 import iudx.apd.acl.server.common.ResponseUrn;
 
 public class ResponseUtil {
-  public static JsonObject generateResponse(HttpStatusCode statusCode, ResponseUrn urn) {
-    return generateResponse(statusCode, urn, statusCode.getDescription());
-  }
+    public static JsonObject generateResponse(HttpStatusCode statusCode, ResponseUrn urn) {
+        return generateResponse(statusCode, urn, statusCode.getDescription());
+    }
 
-  public static JsonObject generateResponse(
-    HttpStatusCode statusCode, ResponseUrn urn, String message) {
-    String type = urn.getUrn();
 
-    return new RestResponse.Builder()
-      .withMessage(message)
-      .withType(type)
-      .withTitle(statusCode.getDescription())
-      .build()
-      .toJson();
-  }
+    public static JsonObject generateResponse(
+            HttpStatusCode statusCode, ResponseUrn urn, String message) {
+        String type = urn.getUrn();
+
+        return new RestResponse.Builder()
+                .withMessage(message)
+                .withType(type)
+                .withTitle(statusCode.getDescription())
+                .build()
+                .toJson();
+    }
 }
