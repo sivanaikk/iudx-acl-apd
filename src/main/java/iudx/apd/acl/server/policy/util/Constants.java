eeb06931aa84787aa1b788716da066e04c8ef3c7
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-08-08 10:41:01 +0530

    Resolve conflicts

--- src/main/java/iudx/apd/acl/server/policy/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/policy/util/Constants.java
@@ -4,14 +4,14 @@ public class Constants {
   public static final int DB_RECONNECT_ATTEMPTS = 2;
   public static final long DB_RECONNECT_INTERVAL_MS = 10;
   public static final String CHECK_EXISTING_POLICY =
-      "select _id from policy where item_id =$1::UUID and item_type = $2::_item_type and owner_id = $3::UUID "
-          + " and status = $4::status_type and user_emailid = $5::text and expiry_at > now() ";
+          "select _id from policy where item_id =$1::UUID and item_type = $2::_item_type and owner_id = $3::UUID "
+                  + " and status = $4::status_type and user_emailid = $5::text and expiry_at > now() ";
   public static final String ENTITY_TABLE_CHECK =
-      "Select _id,provider_id from resource_entity where _id = ANY ($1::UUID[]);";
+          "Select _id,provider_id from resource_entity where _id = ANY ($1::UUID[]);";
 
   public static final String CREATE_POLICY_QUERY =
-      "insert into policy (user_emailid, item_id, owner_id,item_type,expiry_at, constraints,status) "
-          + "values ($1, $2, $3, $4, $5, $6,'ACTIVE') returning *;";
+          "insert into policy (user_emailid, item_id, owner_id,item_type,expiry_at, constraints,status) "
+                  + "values ($1, $2, $3, $4, $5, $6,'ACTIVE') returning *;";
 
   public static final String DELETE_POLICY_QUERY =
           "UPDATE policy SET status='DELETED' WHERE _id = ANY ($1::uuid[])";
@@ -19,9 +19,9 @@ public class Constants {
   public static final String COUNT_OF_ACTIVE_POLICIES =
           COUNT_QUERY + "WHERE _id = ANY($1::uuid[]) AND owner_id=$2::uuid AND status='ACTIVE' AND expiry_at > NOW()";
   public static final String SELECT_QUERY_4_PROVIDER_QUERY =
-          "SELECT P._id AS policy_id, P.owner_id AS owner_id, P.item_id AS item_id, P.item_type AS item_type, P.user_emailid AS consumer_email_id, P.owner_id AS owner_id, U.first_name AS consumer_first_name, U.last_name AS consumer_last_name, P.status AS status, P.expiry_at AS expiry_at, P.created_at AS created_at, P.updated_at AS updated_at";
+          "SELECT P._id AS \"policyId\", P.item_id AS \"itemId\", P.item_type AS \"itemType\", P.user_emailid AS \"consumerEmailId\", U.first_name AS \"consumerFirstName\", U.last_name AS \"consumerLastName\", U._id AS \"consumerId\", P.status AS \"status\", P.expiry_at AS \"expiryAt\", P.constraints AS \"constraints\"";
   public static final String GET_POLICY_4_PROVIDER_QUERY =  SELECT_QUERY_4_PROVIDER_QUERY + " FROM policy AS P INNER JOIN user_table AS U ON P.user_emailid = U.email_id AND P.owner_id = $1;";
-  public static final String SELECT_QUERY_4_CONSUMER_QUERY = "SELECT P._id AS policy_id, P.user_emailid AS consumer_email_id, P.item_id AS item_id, P.item_type AS item_type, P.owner_id AS owner_id, U.first_name AS owner_first_name,U.last_name AS owner_last_name, U.email_id AS owner_email_id, P.status as status, P.expiry_at AS expiry_at, P.created_at AS created_at, P.updated_at AS updated_at";
+  public static final String SELECT_QUERY_4_CONSUMER_QUERY = "SELECT P._id AS \"policyId\", P.item_id AS \"itemId\", P.item_type AS \"itemType\", P.owner_id AS \"ownerId\", U.first_name AS \"ownerFirstName\",U.last_name AS \"ownerLastName\", U.email_id AS \"ownerEmailId\", U._id AS \"ownerId\", P.status as \"status\", P.expiry_at AS \"expiryAt\", P.constraints AS \"constraints\"";
   public static final String GET_POLICY_4_CONSUMER_QUERY = SELECT_QUERY_4_CONSUMER_QUERY + " FROM policy AS P INNER JOIN user_table AS U ON P.owner_id = U._id AND P.user_emailid = $1;";
 
 }
