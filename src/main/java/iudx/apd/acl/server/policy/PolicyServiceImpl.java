e83e2c536c2d8c517da54c61bf33ea18afa4e2e5
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-08-08 10:41:01 +0530

    Resolve conflicts

--- src/main/java/iudx/apd/acl/server/policy/PolicyServiceImpl.java
+++ src/main/java/iudx/apd/acl/server/policy/PolicyServiceImpl.java
@@ -25,21 +25,23 @@ public class PolicyServiceImpl implements PolicyService {
 
     @Override
     public Future<JsonObject> createPolicy(JsonObject request, User user) {
-      Promise<JsonObject> promise = Promise.promise();
+        Promise<JsonObject> promise = Promise.promise();
 
-      request.put("defaultExpiryDays", config.getLong("defaultExpiryDays"));
-      createPolicy
-        .initiateCreatePolicy(request,user)
-        .onComplete(
-          handler -> {
-            if (handler.succeeded()) {
-              promise.complete(handler.result());
-            } else {
-              promise.fail(handler.cause().getMessage());
-            }
-          });
-      return promise.future();
+        request.put("defaultExpiryDays", config.getLong("defaultExpiryDays"));
+        createPolicy
+                .initiateCreatePolicy(request,user)
+                .onComplete(
+                        handler -> {
+                            if (handler.succeeded()) {
+                                promise.complete(handler.result());
+                            } else {
+                                promise.fail(handler.cause().getMessage());
+                            }
+                        });
+        return promise.future();
     }
+
+
 
     @Override
     public Future<JsonObject> deletePolicy(JsonArray policyList, User user) {
