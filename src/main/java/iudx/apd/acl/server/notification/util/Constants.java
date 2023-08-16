da507dffb669a6046a7892d54607e1edfd60dcfa
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-08-16 14:29:48 +0530

    Add update notification

--- src/main/java/iudx/apd/acl/server/notification/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/notification/util/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String WITHDRAW_REQUEST = "UPDATE request SET status = 'WITHDRAWN' WHERE _id = $1::uuid AND expiry_at > NOW() RETURNING _id";
     public static final String GET_REQUEST = "SELECT * FROM request WHERE _id = $1::uuid";
+    public static final String REJECT_NOTIFICATION =  "UPDATE request SET status = 'REJECTED' WHERE _id = $1::uuid AND expiry_at > NOW() RETURNING _id";
+
 }
