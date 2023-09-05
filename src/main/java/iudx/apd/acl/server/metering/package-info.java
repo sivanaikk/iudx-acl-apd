4448121d907d00bd9c76e7af4c76e96ab36958f7
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-09-05 12:23:57 +0530

    Resolve conflict

---
+++ src/main/java/iudx/apd/acl/server/metering/package-info.java
@@ -0,0 +1,5 @@
+@ModuleGen(groupPackage = "iudx.apd.acl.server.metering",
+    name = "iudx-apd-acl-server-metering", useFutures = true)
+package iudx.apd.acl.server.metering;
+
+import io.vertx.codegen.annotations.ModuleGen;
