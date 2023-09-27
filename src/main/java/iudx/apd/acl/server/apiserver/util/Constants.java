ab8b3681c95facb8d7a4731f378f156307d406fd
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-09-27 11:14:07 +0530

    Update to resourceServerRegURL

--- src/main/java/iudx/apd/acl/server/apiserver/util/Constants.java
+++ src/main/java/iudx/apd/acl/server/apiserver/util/Constants.java
@@ -102,6 +102,7 @@ public class Constants {
   public static final String OWNER_ID = "ownerUserId";
   public static final String RESOURCE_TAG = "iudx:ResourceServer";
   public static final String KEY_RESOURCE_SERVER_URL = "resourceServerURL";
+  public static final String RS_URL = "resourceServerRegURL";
 
   public static final String FIRST_NAME = "firstName";
   public static final String LAST_NAME = "lastName";
