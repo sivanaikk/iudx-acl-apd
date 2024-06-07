70e76c214a020b5da124686c0a1cfccd1992778d
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-06-07 15:37:56 +0530

    Update files
    Added APD URL and resource group restriction  based check in CatalogueClient class. Updated the unit tests and open api specs accordingly

--- src/main/java/iudx/apd/acl/server/policy/CatalogueClient.java
+++ src/main/java/iudx/apd/acl/server/policy/CatalogueClient.java
@@ -1,8 +1,7 @@
 package iudx.apd.acl.server.policy;
 
 import static iudx.apd.acl.server.apiserver.util.Constants.*;
-import static iudx.apd.acl.server.common.HttpStatusCode.BAD_REQUEST;
-import static iudx.apd.acl.server.common.HttpStatusCode.INTERNAL_SERVER_ERROR;
+import static iudx.apd.acl.server.common.HttpStatusCode.*;
 
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
@@ -19,6 +18,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
+import iudx.apd.acl.server.validation.exceptions.DxRuntimeException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,14 +29,16 @@ public class CatalogueClient implements CatalogueClientInterface {
   private final String catHost;
   private final Integer catPort;
   private final String catRelationShipPath;
+  private final String apdURL;
   WebClient client;
 
   public CatalogueClient(JsonObject options) {
     WebClientOptions clientOptions =
-        new WebClientOptions().setSsl(true).setVerifyHost(false).setTrustAll(true);
+            new WebClientOptions().setSsl(true).setVerifyHost(false).setTrustAll(true);
     if (this.client == null) {
       this.client = WebClient.create(Vertx.vertx(), clientOptions);
     }
+    this.apdURL = options.getString(APD_URL);
     this.catHost = options.getString("catServerHost");
     this.catPort = options.getInteger("catServerPort");
     this.catRelationShipPath = options.getString("dxCatalogueBasePath") + RELATIONSHIP_PATH;
@@ -47,94 +50,119 @@ public class CatalogueClient implements CatalogueClientInterface {
     List<ResourceObj> resourceObjList = new ArrayList<>();
     for (UUID id : ids) {
       client
-          .get(catPort, catHost, catRelationShipPath)
-          .addQueryParam(ID, String.valueOf(id))
-          .addQueryParam("rel", "all")
-          .send()
-          .onFailure(
-              ar -> {
-                LOGGER.error("fetchItem error : " + ar.getMessage());
-                promise.fail(INTERNAL_SERVER_ERROR.getDescription());
-              })
-          .onSuccess(
-              catSuccessResponse -> {
-                JsonObject resultBody = catSuccessResponse.bodyAsJsonObject();
-                if (resultBody.getString(TYPE).equals(CAT_SUCCESS_URN)) {
-                  List<JsonObject> resultJsonList =
-                      resultBody.getJsonArray(RESULTS).stream()
-                          .map(obj -> (JsonObject) obj)
-                          .collect(Collectors.toList());
-                  UUID provider = null;
-                  UUID resourceGroup = null;
-                  String resServerUrl = null;
-                  boolean isItemGroupLevelResource = false;
-                  boolean isProviderId = false;
-                  boolean isResourceServerId = false;
-                  boolean isInvalidId = false;
+              .get(catPort, catHost, catRelationShipPath)
+              .addQueryParam(ID, String.valueOf(id))
+              .addQueryParam("rel", "all")
+              .send()
+              .onFailure(
+                      ar -> {
+                        LOGGER.error("fetchItem error : " + ar.getMessage());
+                        promise.fail(INTERNAL_SERVER_ERROR.getDescription());
+                      })
+              .onSuccess(
+                      catSuccessResponse -> {
+                        JsonObject resultBody = catSuccessResponse.bodyAsJsonObject();
+                        LOGGER.info(resultBody.encodePrettily());
+                        if (resultBody.getString(TYPE).equals(CAT_SUCCESS_URN)) {
+                          List<JsonObject> resultJsonList =
+                                  resultBody.getJsonArray(RESULTS).stream()
+                                          .map(obj -> (JsonObject) obj)
+                                          .collect(Collectors.toList());
+                          UUID provider = null;
+                          UUID resourceGroup = null;
+                          String resServerUrl = null;
+                          boolean isItemGroupLevelResource = false;
+                          boolean isProviderId = false;
+                          boolean isResourceServerId = false;
+                          boolean isInvalidId = false;
+                          String apdUrlOfResource = "";
 
-                  for (JsonObject resultJson : resultJsonList) {
-                    String type = resultJson.getString(TYPE);
-                    String idFromResponse = resultJson.getString(ID);
-                    /* check if the id being sent is a provider id*/
-                    if (type.contains(PROVIDER_TAG)) {
-                      isProviderId = idFromResponse.equals(id.toString());
-                    }
-                    /* check if the id being sent is a resource server id*/
-                    if (type.contains(RESOURCE_SERVER_TAG)) {
-                      isResourceServerId = idFromResponse.equals(id.toString());
-                    }
+                          for (JsonObject resultJson : resultJsonList) {
+                            String type = resultJson.getString(TYPE);
+                            String idFromResponse = resultJson.getString(ID);
+                            /* check if the id being sent is a provider id*/
+                            if (type.contains(PROVIDER_TAG)) {
+                              isProviderId = idFromResponse.equals(id.toString());
+                            }
+                            /* check if the id being sent is a resource server id*/
+                            if (type.contains(RESOURCE_SERVER_TAG)) {
+                              isResourceServerId = idFromResponse.equals(id.toString());
+                            }
 
-                    isInvalidId = isProviderId || isResourceServerId;
-                    if (!isInvalidId
-                        && idFromResponse != null
-                        && idFromResponse.equals(id.toString())) {
-                      List<String> tags = Util.toList(resultJson.getJsonArray(TYPE));
-                      isItemGroupLevelResource = tags.contains(RESOURCE_GROUP_TAG);
-                    }
+                            isInvalidId = isProviderId || isResourceServerId;
+                            if (!isInvalidId
+                                    && idFromResponse != null
+                                    && idFromResponse.equals(id.toString())) {
+                              List<String> tags = Util.toList(resultJson.getJsonArray(TYPE));
+                              isItemGroupLevelResource = tags.contains(RESOURCE_GROUP_TAG);
+                            }
 
-                    JsonArray typeArray = resultJson.getJsonArray(TYPE);
-                    if (typeArray.contains(RESOURCE_GROUP_TAG)) {
-                      resourceGroup = UUID.fromString(idFromResponse);
-                    } else if (typeArray.contains(PROVIDER_TAG)) {
-                      provider = UUID.fromString(resultJson.getString(OWNER_ID));
-                    } else if (typeArray.contains(RESOURCE_TAG)) {
-                      resServerUrl = resultJson.getString(RS_URL);
-                    }
-                  }
-                  boolean isInfoFromCatInvalid =
-                      id == null || provider == null || resServerUrl == null;
-                  if (isInfoFromCatInvalid && !isInvalidId) {
-                    LOGGER.error("Something from catalogue is null. The resourceId is {}", id);
-                    LOGGER.error("The ownerId is {}", provider);
-                    LOGGER.error("The resource server URL is {}", resServerUrl);
-                    JsonObject failureMessage =
-                        new JsonObject()
-                            .put(TYPE, INTERNAL_SERVER_ERROR.getValue())
-                            .put(TITLE, ResponseUrn.INTERNAL_SERVER_ERROR.getUrn())
-                            .put(
-                                DETAIL,
-                                "Something went wrong while fetching resource info from Catalogue");
-                    promise.fail(failureMessage.encode());
-                  } else if (isProviderId || isResourceServerId) {
-                    LOGGER.error("isProviderId: {}", isProviderId);
-                    LOGGER.error("isResourceServerId: {}", isResourceServerId);
-                    JsonObject failureMessage =
-                        new JsonObject()
-                            .put(TYPE, BAD_REQUEST.getValue())
-                            .put(TITLE, ResponseUrn.BAD_REQUEST_URN.getUrn())
-                            .put(DETAIL, "Given id is invalid");
-                    promise.fail(failureMessage.encode());
-                  } else {
-                    ResourceObj resourceObj =
-                        new ResourceObj(
-                            id, provider, resourceGroup, resServerUrl, isItemGroupLevelResource);
-                    resourceObjList.add(resourceObj);
-                    promise.complete(resourceObjList);
-                  }
-                } else {
-                  promise.fail(resultBody.getString(DETAIL));
-                }
-              });
+                            JsonArray typeArray = resultJson.getJsonArray(TYPE);
+                            if (typeArray.contains(RESOURCE_GROUP_TAG)) {
+                              resourceGroup = UUID.fromString(idFromResponse);
+                            } else if (typeArray.contains(PROVIDER_TAG)) {
+                              provider = UUID.fromString(resultJson.getString(OWNER_ID));
+                            } else if (typeArray.contains(RESOURCE_TAG)) {
+                              resServerUrl = resultJson.getString(RS_URL);
+                            } else if (typeArray.contains(RESOURCE_ITEM_TAG))
+                            {
+                              apdUrlOfResource = resultJson.getString(APD_URL);
+                            }
+                          }
+                          boolean isInfoFromCatInvalid =
+                                  id == null || provider == null || resServerUrl == null;
+                          if (isInfoFromCatInvalid && !isInvalidId) {
+                            LOGGER.error("Something from catalogue is null. The resourceId is {}", id);
+                            LOGGER.error("The ownerId is {}", provider);
+                            LOGGER.error("The resource server URL is {}", resServerUrl);
+                            JsonObject failureMessage =
+                                    new JsonObject()
+                                            .put(TYPE, INTERNAL_SERVER_ERROR.getValue())
+                                            .put(TITLE, ResponseUrn.INTERNAL_SERVER_ERROR.getUrn())
+                                            .put(
+                                                    DETAIL,
+                                                    "Something went wrong while fetching resource info from Catalogue");
+                            promise.fail(failureMessage.encode());
+                          } else if (isProviderId || isResourceServerId) {
+                            LOGGER.error("isProviderId: {}", isProviderId);
+                            LOGGER.error("isResourceServerId: {}", isResourceServerId);
+                            JsonObject failureMessage =
+                                    new JsonObject()
+                                            .put(TYPE, BAD_REQUEST.getValue())
+                                            .put(TITLE, ResponseUrn.BAD_REQUEST_URN.getUrn())
+                                            .put(DETAIL, "Given id is invalid - it is a provider or resource server id");
+                            promise.fail(failureMessage.encode());
+                          } else if(isItemGroupLevelResource)
+                          {
+
+                            JsonObject failureMessage =
+                                    new JsonObject()
+                                            .put(TYPE, BAD_REQUEST.getValue())
+                                            .put(TITLE, ResponseUrn.BAD_REQUEST_URN.getUrn())
+                                            .put(DETAIL, "Given id is invalid - it is group level resource");
+                            promise.fail(failureMessage.encode());
+                          }
+                          /* if the resource has an APD URL that is not equal to the current APD URL*/
+                          else if(!isItemGroupLevelResource && !apdURL.equals(apdUrlOfResource))
+                          {
+                            JsonObject failureMessage =
+                                    new JsonObject()
+                                            .put(TYPE, FORBIDDEN.getValue())
+                                            .put(TITLE, ResponseUrn.FORBIDDEN_URN.getUrn())
+                                            .put(DETAIL, "Resource is forbidden to access, as the APD URL for the resource is different than the current APD");
+                            promise.fail(failureMessage.encode());
+                          }
+                          else {
+                            ResourceObj resourceObj =
+                                    new ResourceObj(
+                                            id, provider, resourceGroup, resServerUrl, isItemGroupLevelResource);
+                            resourceObjList.add(resourceObj);
+                            promise.complete(resourceObjList);
+                          }
+                        } else {
+                          promise.fail(resultBody.getString(DETAIL));
+                        }
+                      });
     }
     return promise.future();
   }
