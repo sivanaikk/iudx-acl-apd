92e88c357dc468879f9acf79744a03a20b0c4b2f
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-06-07 16:15:16 +0530

    Update postman collection

--- src/test/java/iudx/apd/acl/server/policy/TestCatalogueClient.java
+++ src/test/java/iudx/apd/acl/server/policy/TestCatalogueClient.java
@@ -29,7 +29,6 @@ import iudx.apd.acl.server.common.ResponseUrn;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
