fd251cdcf691bfe739a96104b6728f412a9cfbaa
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-06-11 15:31:53 +0530

    Test commit

--- src/test/java/iudx/apd/acl/server/policy/TestCreatePolicy.java
+++ src/test/java/iudx/apd/acl/server/policy/TestCreatePolicy.java
@@ -26,6 +26,7 @@ import java.util.UUID;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,7 +35,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
+@Disabled
 @Testcontainers
 @ExtendWith({MockitoExtension.class, VertxExtension.class})
 public class TestCreatePolicy {
